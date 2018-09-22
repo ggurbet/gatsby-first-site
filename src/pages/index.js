42a59308d2ba06bfe3d6aaf5bd80cde63b6954bc
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2018-09-22 14:29:29 +0300

    Initial commit
    Initial commit with initialization of Gatsby new site.

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello, World!</h1>
+    <p>Here is my first website with Gatsby.</p>
+    <p>Enjoy the sleek and cozy atmosphere.</p>
+  </Layout>
+)
+
+export default IndexPage
